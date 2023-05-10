cf5237cb6607f41ed864eee012533d4ff760e0ad
tamzid620 <tamzid620@gmail.com>
2023-05-10 11:21:18 +0600

    first commit

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Home/Shared/Footer/Footer";
+import Navbar from "../Pages/Home/Home/Shared/Navbar/Navbar";
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Layout;
