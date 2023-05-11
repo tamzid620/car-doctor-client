cfb880fce09282e666e56dffed22825b4bda51b5
tamzid620 <tamzid620@gmail.com>
2023-05-11 19:19:24 +0600

    final commmit for mongodb

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,41 +1,43 @@
 
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
-// import CheckOut from "../Pages/CheckOut/CheckOut";
 import BookService from "../Pages/BookService/BookService";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Bookings from "../Pages/Bookings/Bookings";
+import PrivateRoute from "./PrivateRoutes";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout></Layout>,
-      children:[
-        {
-            path: '/',
-            element: <Home></Home>,
-        },
-        {
-            path: '/login',
-            element: <Login></Login>,
-        },
-        {
-            path: '/signup',
-            element: <SignUp></SignUp>,
-        },
-        {
-            path: '/bookservice/:id',
-            element: <BookService></BookService>,
-            loader:({params}) => fetch(`http://localhost:5000/services/${params.id}`)
-        },
-        // {
-        //     path: '/checkout/:id',
-        //     element: <CheckOut></CheckOut>,
-        //     loader:({params}) => fetch(`http://localhost:5000/services/${params.id}`)
-        // },
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>,
+      },
+      {
+        path: '/signup',
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: '/bookservice/:id',
+        element: <BookService></BookService>,
+        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+      },
+      {
+        path: '/bookings',
+        element: <PrivateRoute><Bookings></Bookings></PrivateRoute>,
+      },
+    ]
+  },
+]);
 
 export default router
